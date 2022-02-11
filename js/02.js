b6ca2bdf09794fd6c6abd83376df57a175ad078c
Evgeny Ruban <ruban.z1@mail.ru>
2022-02-11 18:25:00 +0300

    add twotasks

--- js/02.js
+++ js/02.js
@@ -2,3 +2,12 @@
 const myName = "Zhenya";
 const myStr = 'My name is ' + myName + ' and i am well!';
 console.log('test 2');
+
+const mySstr = 'This is the start. ' + 'This is the end.';// Change this line
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName; // Change this line
+firstLetterOfLastName = lastName[0];
