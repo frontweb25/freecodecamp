864a4f43ecf454ec3055e030e5ed7ae67e8369d0
Evgeny Ruban <ruban.z1@mail.ru>
2022-02-11 18:14:49 +0300

    add console.log

--- js/02.js
+++ js/02.js
@@ -1,3 +1,4 @@
 // Only change code below this line
 const myName = "Zhenya";
-const myStr = 'My name is ' + myName + ' and I am well!';
+const myStr = 'My name is ' + myName + ' and i am well!';
+console.log('test 2');
